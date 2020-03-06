3e50f7e652e85bf499ec7673ba87246c07461cac
Mateusz <mateuszd2411@gmail.com>
2020-03-06 11:03:03 +0100

    I added a list of categories

---
+++ app/src/main/java/com/example/quizapp/CategoryModel.java
@@ -0,0 +1,28 @@
+package com.example.quizapp;
+
+public class CategoryModel {
+
+    private String imageUrl, title;
+
+    public CategoryModel(String imageUrl, String title) {
+        this.imageUrl = imageUrl;
+        this.title = title;
+    }
+
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
