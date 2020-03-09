db2618dfeccdc2365e51ea7637a267d82bc07a66
Mateusz <mateuszd2411@gmail.com>
2020-03-09 16:28:23 +0100

    adding bookmarks to bookmark main

--- app/src/main/java/com/example/quizapp/BookmarkActivity.java
+++ app/src/main/java/com/example/quizapp/BookmarkActivity.java
@@ -6,16 +6,31 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.quizapp.QuestionsActivity.FILE_NAME;
+import static com.example.quizapp.QuestionsActivity.KEY_NAME;
 
 public class BookmarkActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+
+    private List<QuestionModel> bookmarksList;
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,24 +44,28 @@ public class BookmarkActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.rv_bookmarks);
 
+        preferences = getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        editor = preferences.edit();
+        gson = new Gson();
+
+        getBookmarks();
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(RecyclerView.VERTICAL);
 
         recyclerView.setLayoutManager(layoutManager);
 
-        List<QuestionModel> list = new ArrayList<>();
-        list.add(new QuestionModel("what is your name?","","","","","Mateusz",0));
-        list.add(new QuestionModel("what is your name?","","","","","Mateusz",0));
-        list.add(new QuestionModel("what is your name?","","","","","Mateusz",0));
-        list.add(new QuestionModel("what is your name?","","","","","Mateusz",0));
-        list.add(new QuestionModel("what is your name?","","","","","Mateusz",0));
-        list.add(new QuestionModel("what is your name?","","","","","Mateusz",0));
-        list.add(new QuestionModel("what is your name?","","","","","Mateusz",0));
-        list.add(new QuestionModel("what is your name?","","","","","Mateusz",0));
 
-        BookmarksAdapter adapter = new BookmarksAdapter(list);
+        BookmarksAdapter adapter = new BookmarksAdapter(bookmarksList);
         recyclerView.setAdapter(adapter);
 
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        storeBookmarks();
     }
 
     @Override
@@ -57,5 +76,26 @@ public class BookmarkActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void getBookmarks(){
+
+        String json = preferences.getString(KEY_NAME, "");
+
+        Type type = new TypeToken<List<QuestionModel>>(){}.getType();
+
+        bookmarksList = gson.fromJson(json, type);
+
+        if (bookmarksList == null){
+            bookmarksList = new ArrayList<>();
+        }
+    }
+
+    private void storeBookmarks(){
+
+        String json = gson.toJson(bookmarksList);
+        editor.putString(KEY_NAME,json);
+        editor.commit();
     }
 }
